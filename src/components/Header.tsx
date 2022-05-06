b2bd0a8c25d8de1fc9070d3ff2db8554fe3f8858
viniciosComerciante <vinicompif@gmail.com>
2022-05-06 00:00:17 -0300

    feat: sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,9 @@
-import { Flex, Text, Input, Icon } from "@chakra-ui/react";
-import { RiSearchLine } from "react-icons/ri";
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from "@chakra-ui/react";
+import {
+  RiNotificationLine,
+  RiSearchLine,
+  RiUserAddLine,
+} from "react-icons/ri";
 
 export default function Header() {
   return (
@@ -41,6 +45,33 @@ export default function Header() {
           mr="4"
         ></Input>
         <Icon as={RiSearchLine} fontSize={20}></Icon>
+      </Flex>
+      <Flex align="center" ml="auto">
+        <HStack
+          spacing="8"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20"></Icon>
+          <Icon as={RiUserAddLine} fontSize="20"></Icon>
+        </HStack>
+        <Flex align="center">
+          <Box mr="4" textAlign="right">
+            <Text>Vinicios Oliveira</Text>
+            <Text color="gray.300" fontSize="small">
+              vinicompif@gmail.com
+            </Text>
+          </Box>
+          <Avatar
+            size="md"
+            name="vinicios oliveira"
+            src="https://github.com/viniciosComerciante.png"
+          ></Avatar>
+        </Flex>
       </Flex>
     </Flex>
   );
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,14 @@
+import { Flex } from "@chakra-ui/react";
 import Header from "../components/Header";
+import { SideBar } from "../components/Sidebar";
 
 export default function Dashboard() {
-  return <Header></Header>;
+  return (
+    <Flex direction="column" h="100vh">
+      <Header></Header>
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <SideBar></SideBar>
+      </Flex>
+    </Flex>
+  );
 }
