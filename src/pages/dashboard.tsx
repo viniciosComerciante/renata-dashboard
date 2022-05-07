6bafd2354a05d4b3805ad87f43fab9dc1f301412
viniciosComerciante <vinicompif@gmail.com>
2022-05-07 12:38:22 -0300

    feat: users list

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,13 +1,99 @@
-import { Flex } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
+import dynamic from "next/dynamic";
 import Header from "../components/Header";
 import { SideBar } from "../components/Sidebar";
+const Chart = dynamic(() => import("react-apexcharts"), {
+  ssr: false,
+});
 
 export default function Dashboard() {
+  const options = {
+    chart: {
+      toolbar: {
+        show: false,
+      },
+      zoom: {
+        enabled: false,
+      },
+      foreColor: theme.colors.gray[500],
+    },
+    grid: {
+      show: false,
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    tooltip: {
+      enabled: false,
+    },
+    xaxis: {
+      type: "datetime",
+      axysBorder: {
+        color: theme.colors.gray[600],
+      },
+      axysTicks: {
+        color: theme.colors.gray[600],
+      },
+      categories: [
+        "2021-03-18T00:00:00:000Z",
+        "2021-03-19T00:00:00:000Z",
+        "2021-03-20T00:00:00:000Z",
+        "2021-03-21T00:00:00:000Z",
+        "2021-03-22T00:00:00:000Z",
+        "2021-03-23T00:00:00:000Z",
+        "2021-03-24T00:00:00:000Z",
+      ],
+    },
+    fill: {
+      opacity: 0.3,
+      type: "gradient",
+      gradient: {
+        shade: "dark",
+        opacityFrom: 0.7,
+        opacityTo: 0.3,
+      },
+    },
+  };
+  const series = [
+    {
+      name: "series1",
+      data: [31, 120, 10, 28, 61, 18, 109],
+    },
+  ];
   return (
     <Flex direction="column" h="100vh">
       <Header></Header>
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <SideBar></SideBar>
+        <SimpleGrid
+          flex="1"
+          gap="4"
+          minChildWidth="320px"
+          alignItems="flex-start"
+        >
+          <Box bg="gray.800" p="8" borderRadius={8} pb="4">
+            <Text fontSize="large" mb="4">
+              Inscritos da Semana
+            </Text>
+            <Chart
+              type="area"
+              height={160}
+              options={options}
+              series={series}
+            ></Chart>
+          </Box>
+          <Box bg="gray.800" p="8" borderRadius={8} pb="4">
+            <Text fontSize="large" mb="4">
+              Taxa de Abertura
+            </Text>
+            <Chart
+              type="area"
+              height={160}
+              options={options}
+              series={series}
+            ></Chart>
+          </Box>
+        </SimpleGrid>
       </Flex>
     </Flex>
   );
---
+++ src/pages/users/index.tsx
@@ -0,0 +1,241 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import Header from "../../components/Header";
+import { SideBar } from "../../components/Sidebar";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header></Header>
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <SideBar></SideBar>
+        <Box flex="1" borderRadius={8} bg="gray.800" padding="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+            </Heading>
+            <Button
+              as="a"
+              size="sm"
+              fontSize="small"
+              colorScheme="pink"
+              leftIcon={<Icon fontSize="20" as={RiAddLine} />}
+            >
+              Criar novo usuário
+            </Button>
+          </Flex>
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink"></Checkbox>
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de cadastro</Th>
+                <Th width="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink"></Checkbox>
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Vinicios Oliveira</Text>
+                    <Text fontWeight="normal" fontSize="sm" color="gray.300">
+                      vinicompif@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>04 de Abril, 2021</Td>
+                <Td>
+                  {" "}
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="small"
+                    colorScheme="purple"
+                    leftIcon={<Icon fontSize="16" as={RiPencilLine} />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink"></Checkbox>
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Vinicios Oliveira</Text>
+                    <Text fontWeight="normal" fontSize="sm" color="gray.300">
+                      vinicompif@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>04 de Abril, 2021</Td>
+                <Td>
+                  {" "}
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="small"
+                    colorScheme="purple"
+                    leftIcon={<Icon fontSize="16" as={RiPencilLine} />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink"></Checkbox>
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Vinicios Oliveira</Text>
+                    <Text fontWeight="normal" fontSize="sm" color="gray.300">
+                      vinicompif@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>04 de Abril, 2021</Td>
+                <Td>
+                  {" "}
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="small"
+                    colorScheme="purple"
+                    leftIcon={<Icon fontSize="16" as={RiPencilLine} />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink"></Checkbox>
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Vinicios Oliveira</Text>
+                    <Text fontWeight="normal" fontSize="sm" color="gray.300">
+                      vinicompif@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>04 de Abril, 2021</Td>
+                <Td>
+                  {" "}
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="small"
+                    colorScheme="purple"
+                    leftIcon={<Icon fontSize="16" as={RiPencilLine} />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink"></Checkbox>
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Vinicios Oliveira</Text>
+                    <Text fontWeight="normal" fontSize="sm" color="gray.300">
+                      vinicompif@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>04 de Abril, 2021</Td>
+                <Td>
+                  {" "}
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="small"
+                    colorScheme="purple"
+                    leftIcon={<Icon fontSize="16" as={RiPencilLine} />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>{" "}
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink"></Checkbox>
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Vinicios Oliveira</Text>
+                    <Text fontWeight="normal" fontSize="sm" color="gray.300">
+                      vinicompif@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>04 de Abril, 2021</Td>
+                <Td>
+                  {" "}
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="small"
+                    colorScheme="purple"
+                    leftIcon={<Icon fontSize="16" as={RiPencilLine} />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>{" "}
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink"></Checkbox>
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Vinicios Oliveira</Text>
+                    <Text fontWeight="normal" fontSize="sm" color="gray.300">
+                      vinicompif@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>04 de Abril, 2021</Td>
+                <Td>
+                  {" "}
+                  <Button
+                    as="a"
+                    size="sm"
+                    fontSize="small"
+                    colorScheme="purple"
+                    leftIcon={<Icon fontSize="16" as={RiPencilLine} />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  );
+}
