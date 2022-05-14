a8460fab21a348ec0b9ede57ed5235c0c260e88f
viniciosComerciante <vinicompif@gmail.com>
2022-05-14 15:09:45 -0300

    Feat: Header Responsivity

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,23 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, useBreakpointValue } from "@chakra-ui/react";
 import { Logo } from "./Logo";
 import { NotificationsNav } from "./NotificationsNav";
 import { Profile } from "./Profile";
 import { SearchBox } from "./SearchBox";
 
 export default function Header() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <Flex w="100%" as="header" maxWidth={1480} h="20" mx="auto" mt="4" px="6">
       <Logo></Logo>
-      <SearchBox></SearchBox>
+      {isWideVersion && <SearchBox></SearchBox>}
+
       <Flex align="center" ml="auto">
         <NotificationsNav></NotificationsNav>
-        <Profile></Profile>
+        <Profile showProfileData={isWideVersion}></Profile>
       </Flex>
     </Flex>
   );
