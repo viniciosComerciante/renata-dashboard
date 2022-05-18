ead4ffe9da84f9f8dd9bb295939d477ae0790f60
viniciosComerciante <vinicompif@gmail.com>
2022-05-17 23:24:09 -0300

    sidebar responsivity

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,14 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
+import { RiMenuLine } from "react-icons/ri";
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
 import { Logo } from "./Logo";
 import { NotificationsNav } from "./NotificationsNav";
 import { Profile } from "./Profile";
 import { SearchBox } from "./SearchBox";
 
 export default function Header() {
+  const { onOpen } = useSidebarDrawer();
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -12,6 +16,17 @@ export default function Header() {
 
   return (
     <Flex w="100%" as="header" maxWidth={1480} h="20" mx="auto" mt="4" px="6">
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Open Navigation"
+          icon={<Icon as={RiMenuLine}></Icon>}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+          mt="6"
+        ></IconButton>
+      )}
       <Logo></Logo>
       {isWideVersion && <SearchBox></SearchBox>}
 
